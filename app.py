cb5358d4f6cd9a9d2d5afd26a0b2fe44ca9a72ba
GayoungHong <rkdud473@naver.com>
2023-06-12 16:57:31 +0900

    testt

--- app.py
+++ app.py
@@ -22,20 +22,20 @@ den = [1, 5, 6, 100]
 # 폐루프 전달함수 계산
 closed_loop_tf = signal.TransferFunction(num, den) / (1 + signal.TransferFunction(num, den))
 
-    # 전달함수 출력
+# 전달함수 출력
 st.markdown("Closed-Loop Transfer Function:")
 st.latex("T(s) = \\frac{{100}}{{(s+2)(s+3) + 100}}")
-    
-    # Bode plot 그리기
+
+# Bode plot 그리기
 fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 6))
-    
-    # 게인 그래프
+
+# 게인 그래프
 w, mag, phase = signal.bode(closed_loop_tf)
 ax1.semilogx(w, mag)
 ax1.set_ylabel('Gain (dB)')
 ax1.grid(True)
 
-    # 위상 그래프
+# 위상 그래프
 ax2.semilogx(w, phase)
 ax2.set_xlabel('Frequency (rad/s)')
 ax2.set_ylabel('Phase (degrees)')
@@ -44,4 +44,4 @@ ax2.grid(True)
 st.pyplot(fig)
 
 if __name__ == '__main__':
-    main()
+main()
